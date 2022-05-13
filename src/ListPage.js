76a1a9f937eeca228058758fca1834b33db58c22
Cody-Wise <cody@sharptackmedia.com>
2022-05-13 09:56:41 -0700

    Refactored listpage

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,27 +1,42 @@
 import { useState, useEffect } from 'react';
 import { getGames } from './services/fetch-utils';
 import Game from './Game';
+import React from 'react';
 
-export default function ListPage() {
-  const [games, setGames] = useState([]);
+export default class ListPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      games: [],
+    };
+  }
+  // const [games, setGames] = useState([]);
   // you'll need some state to hold onto the array of games
 
-  useEffect(() => {
-    async function load() {
-      const data = await getGames();
+  async componentDidMount() {
+    const data = await getGames();
 
-      setGames(data);
-    }
-    load();
-  }, []);
+    this.setState({ games: data });
+  }
+
+  // useEffect(() => {
+  //   async function load() {
+  //     const data = await getGames();
+
+  //     setGames(data);
+  //   }
+  //   load();
+  // }, []);
 
   // fetch the games on load and inject them into state
-  return (
-    <div className="games">
-      {/* map through the games in state and render Game components */}
-      {games.map((game, i) => (
-        <Game key={game + i} game={game} />
-      ))}
-    </div>
-  );
+  render() {
+    return (
+      <div className="games">
+        {/* map through the games in state and render Game components */}
+        {this.games.map((game, i) => (
+          <Game key={game + i} game={game} />
+        ))}
+      </div>
+    );
+  }
 }
